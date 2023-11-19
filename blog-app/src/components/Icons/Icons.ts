6c08b698ef07a1b2c9e52f784d4b60c9dbeb00c3
Виктория Голубова <v.golubova@denexy.com>
2023-11-19 20:50:07 +0300

    Added AnonIcon

--- blog-app/src/components/Icons/Icons.ts
+++ blog-app/src/components/Icons/Icons.ts
@@ -3,6 +3,7 @@ import ComplainIcon from "./components/ComplainIcon";
 import HideIcon from "./components/HideIcon";
 import SaveIcon from "./components/SaveIcon";
 import ShareIcon from "./components/ShareIcon";
+import AnonIcon from "./components/AnonIcon";
 
 const EIcons = {
         commentIcon: CommentIcon,
@@ -10,6 +11,7 @@ const EIcons = {
         hideIcon: HideIcon,
         saveIcon: SaveIcon,
         shareIcon: ShareIcon,
+        anonIcon: AnonIcon,
 };
 
 export default EIcons;
