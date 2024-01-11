1c19a552d2c7db5635b98d94b4e1f35350a6dd04
Виктория Голубова <v.golubova@denexy.com>
2024-01-11 10:51:08 +0400

    Added request for comments to a post when opening a post and saving them to the state

--- blog-app/src/components/CardsList/Card/TextContent/TextContent.tsx
+++ blog-app/src/components/CardsList/Card/TextContent/TextContent.tsx
@@ -1,8 +1,23 @@
 import avatar from './avatar.png';
 import style from './TextContent.module.css';
 import * as React from "react";
+import {useState} from "react";
+import {Post} from "../../../Post/Post";
+import axios from "axios";
 
-export default function TextContent({data}): React.JSX.Element {
+export default function TextContent({data, resolveLink = true}): React.JSX.Element {
+	const [isModalOpened, setIsModalOpened] = useState(false);
+	const [comments, setComments] = useState([]);
+
+	function handleModalOpen (e) {
+		e.stopPropagation();
+		axios.get(`http://api.reddit.com/r/${data.subreddit}/comments/${data.id}`).then(({ data }) => {
+			const listComments = data.filter((item) => !item.data.dist).map((item) => item.data.children).flat().filter((item) => item.kind !== 'more');
+			setComments(listComments);
+			setIsModalOpened(true);
+        }).catch((e) => console.error(e));
+	}
+
 	return (
 		<div className={style.textContent}>
 			<div className={style.metaData}>
@@ -15,7 +30,10 @@ export default function TextContent({data}): React.JSX.Element {
 					5 часов назад</span>
 			</div>
 			<h2 className={style.title}>
-				<a className={style.postLink} href={data.url}>{data.title}</a>
+				{resolveLink ? <a className={style.postLink} href='#post-url' onClick={handleModalOpen}>{data.title}</a> : <div>{data.title}</div>}
+				{isModalOpened && (
+					<Post onClose={() => setIsModalOpened(false)} data={data} comments={comments} />
+				)}
 			</h2>
 		</div>
 	);
