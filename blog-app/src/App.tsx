3b7a8a30400205b5b29c5b6f03d75799dfafa259
Виктория Голубова <v.golubova@denexy.com>
2024-01-15 18:40:24 +0400

    Deleted tokenContext.Provider

--- blog-app/src/App.tsx
+++ blog-app/src/App.tsx
@@ -4,13 +4,14 @@ import * as React from "react";
 import Header from "./components/Header/Header";
 import axios from "axios";
 import type { InternalAxiosRequestConfig } from 'axios';
-import {useEffect, useState} from "react";
-import {tokenContext} from "./context/tokenContext";
+import {useEffect} from "react";
 import {UserContextProvider} from "./context/userContext";
 import {PostsContextProvider} from "./context/postsContext";
+import {useDispatch} from "react-redux";
+import {setToken} from "./features/token/tokenSlice";
 
 export default function App(): React.JSX.Element {
-    const [token, setToken] = useState('');
+    const dispatch = useDispatch();
     const axiosConfig = { auth: { username: import.meta.env.VITE_CLIENT_ID, password: 'xQJPwicUqjnXixMNK6KCsd2OqCqCCg' },
         headers: { "Content-type": "application/x-www-form-urlencoded" } } as InternalAxiosRequestConfig;
     useEffect(() => {
@@ -20,21 +21,19 @@ export default function App(): React.JSX.Element {
             axios.post('https://www.reddit.com/api/v1/access_token',
                 `grant_type=authorization_code&code=${code}&redirect_uri=http://localhost:5173/auth`,
                 axiosConfig).then(({ data }) => {
-                    setToken(data.access_token);
+                    dispatch(setToken(data.access_token));
             }).catch((e) => console.error(e));
         }
     }, []);
 
     return (
-        <tokenContext.Provider value={token}>
-            <UserContextProvider value={{}}>
-                <PostsContextProvider value={[]}>
-                    <div className = 'App' >
-                        <Header />
-                        <CardsList />
-                    </div>
-                </PostsContextProvider>
-            </UserContextProvider>
-        </tokenContext.Provider>
+        <UserContextProvider value={{}}>
+            <PostsContextProvider value={[]}>
+                <div className = 'App' >
+                    <Header />
+                    <CardsList />
+                </div>
+            </PostsContextProvider>
+        </UserContextProvider>
     );
 }
