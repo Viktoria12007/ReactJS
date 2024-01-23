21744b7ada8fa8f6b8f2971f70835f0780d88466
Виктория Голубова <v.golubova@denexy.com>
2024-01-23 20:18:12 +0400

    In tokenSlice.ts. added async function. Asynchronous token logic moved from components to tokenSlice.ts.

--- blog-app/src/App.tsx
+++ blog-app/src/App.tsx
@@ -2,27 +2,26 @@ import './App.css';
 import CardsList from './components/CardsList/CardsList';
 import * as React from "react";
 import Header from "./components/Header/Header";
-import axios from "axios";
-import type { InternalAxiosRequestConfig } from 'axios';
 import {useEffect} from "react";
-import {UserContextProvider} from "./context/userContext";
 import {PostsContextProvider} from "./context/postsContext";
 import {useDispatch} from "react-redux";
-import {setToken} from "./features/token/tokenSlice";
+import {fetchToken, setToken} from "./features/token/tokenSlice";
+import {InternalAxiosRequestConfig} from "axios";
 
 export default function App(): React.JSX.Element {
     const dispatch = useDispatch();
     const axiosConfig = { auth: { username: import.meta.env.VITE_CLIENT_ID, password: 'xQJPwicUqjnXixMNK6KCsd2OqCqCCg' },
         headers: { "Content-type": "application/x-www-form-urlencoded" } } as InternalAxiosRequestConfig;
+
     useEffect(() => {
-        const url = new URL(window.location.href);
-        if (url.pathname === '/auth') {
-            const code = url.searchParams.get("code");
-            axios.post('https://www.reddit.com/api/v1/access_token',
-                `grant_type=authorization_code&code=${code}&redirect_uri=http://localhost:5173/auth`,
-                axiosConfig).then(({ data }) => {
-                    dispatch(setToken(data.access_token));
-            }).catch((e) => console.error(e));
+        if (!sessionStorage.blogAppToken) {
+            const url = new URL(window.location.href);
+            if (url.pathname === '/auth') {
+                const code = url.searchParams.get("code");
+                dispatch(fetchToken({ code, axiosConfig }));
+            }
+        } else {
+            dispatch(setToken(sessionStorage.blogAppToken));
         }
     }, []);
 
--- blog-app/src/features/token/tokenSlice.ts
+++ blog-app/src/features/token/tokenSlice.ts
@@ -1,11 +1,21 @@
-import {createSlice, PayloadAction} from '@reduxjs/toolkit'
+import {createAsyncThunk, createSlice, PayloadAction} from '@reduxjs/toolkit'
+import axios from "axios";
 
-export interface tokenState {
-    value: string
+export interface IToken {
+    value: string,
+    error: string,
 }
-const initialState: tokenState = {
-    value: ''
+const initialState: IToken = {
+    value: '',
+    error: '',
 };
+
+export const fetchToken = createAsyncThunk('token/fetchToken', async ({ code, axiosConfig }) => {
+    const response = await axios.post('https://www.reddit.com/api/v1/access_token',
+        `grant_type=authorization_code&code=${code}&redirect_uri=http://localhost:5173/auth`,
+        axiosConfig);
+    return response.data;
+});
 
 export const tokenSlice = createSlice({
     name: 'token',
@@ -13,11 +23,24 @@ export const tokenSlice = createSlice({
     reducers: {
         setToken: (state, action: PayloadAction<string>) => {
             state.value = action.payload;
-        }
-    }
+        },
+    },
+    extraReducers(builder) {
+        builder
+            .addCase(fetchToken.fulfilled, (state: IToken, action) => {
+                state.value = action.payload.access_token;
+                sessionStorage.blogAppToken = action.payload.access_token;
+            })
+            .addCase(fetchToken.rejected, (state: IToken, action) => {
+                state.error = action.error.message;
+                console.debug(state.error);
+            })
+
+    },
 })
 
-// Action creators are generated for each case reducer function
-export const { setToken } = tokenSlice.actions
+export const { setToken } = tokenSlice.actions;
+
+export const tokenValue = state => state.token.value;
 
 export default tokenSlice.reducer
