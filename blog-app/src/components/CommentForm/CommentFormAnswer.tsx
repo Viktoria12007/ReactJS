9609930871f3733ca45e1a9d3e9a23be87ca04c6
Виктория Голубова <v.golubova@denexy.com>
2024-01-25 19:20:35 +0400

    Added new states for CommentFormAnswer.tsx

--- blog-app/src/components/CommentForm/CommentFormAnswer.tsx
+++ blog-app/src/components/CommentForm/CommentFormAnswer.tsx
@@ -5,6 +5,8 @@ import {userData} from "../../features/user/userSlice";
 
 export function CommentFormAnswer() {
     const [valueCommentFormAnswer, onChangeCommentFormAnswer] = useState('');
+    const [touched, setTouched] = useState(false);
+    const [valueError, setValueError] = useState('');
     const { name } = useSelector(userData);
     const commentRef = useRef<HTMLTextAreaElement>(null);
     useEffect(() => {
@@ -17,11 +19,28 @@ export function CommentFormAnswer() {
     function handleSubmit(e: FormEvent) {
         e.preventDefault();
         console.log(valueCommentFormAnswer);
+        setTouched(true);
+        setValueError(validateValue());
+        const isFormValid = !validateValue();
+        if (!isFormValid) return;
+        alert('Форма отправлена!');
+    }
+    function validateValue() {
+        if (valueCommentFormAnswer.length <= 3) return 'Введите больше 3-х символов';
+        return '';
     }
 
     return (
         <form className={style.form} onSubmit={handleSubmit}>
-            <textarea ref={commentRef} className={style.input} placeholder={`${name || 'Аноним'}, оставьте ваш комментарий` } value={valueCommentFormAnswer} onChange={handleChange}/>
+            <textarea
+                ref={commentRef}
+                className={style.input}
+                placeholder={`${name || 'Аноним'}, оставьте ваш комментарий` }
+                value={valueCommentFormAnswer}
+                onChange={handleChange}
+                aria-invalid={valueError ? 'true' : undefined}
+            />
+            {touched && valueError && (<div>{valueError}</div>)}
             <button type="submit" className={style.button}>Комментировать</button>
         </form>
     );
